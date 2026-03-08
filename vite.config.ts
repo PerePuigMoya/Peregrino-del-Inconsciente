118eea8806e1e6f8eb2d070d4c659c572a5d4bce
PerePuigMoya <167614185+PerePuigMoya@users.noreply.github.com>
2026-03-08 19:10:00 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,9 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
+    // loadEnv lee los archivos .env locales (útil para tu computadora)
     const env = loadEnv(mode, '.', '');
+    
     return {
       server: {
         port: 3000,
@@ -11,8 +13,9 @@ export default defineConfig(({ mode }) => {
       },
       plugins: [react()],
       define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+        // Priorizamos process.env (Vercel) sobre env (Local)
+        'process.env.API_KEY': JSON.stringify(process.env.GEMINI_API_KEY || env.GEMINI_API_KEY),
+        'process.env.GEMINI_API_KEY': JSON.stringify(process.env.GEMINI_API_KEY || env.GEMINI_API_KEY)
       },
       resolve: {
         alias: {
